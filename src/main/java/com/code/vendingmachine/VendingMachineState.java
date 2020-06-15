6d1da1f684f226651bf8a87ed08a98cf6d156461
preethi <preethi.rao@ecofactor.com>
2020-06-14 22:35:03 -0700

    next commit

--- src/main/java/com/code/vendingmachine/VendingMachineState.java
+++ src/main/java/com/code/vendingmachine/VendingMachineState.java
@@ -1,4 +1,8 @@
 package com.code.vendingmachine;
 
 public interface VendingMachineState {
+    public void collectCash(int cash);
+    public void dispenseChange(String productCode);
+    public void dispenseItem(String productCode);
+    public void cancelTransaction();
 }
